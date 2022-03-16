b3594c86d77dfd56d6afdc0e8a2e3f9ef7d9b9a3
ryanallenk <ryanallenk@gmail.com>
2022-03-16 13:31:59 -0400

    test move commands

--- client.js
+++ client.js
@@ -11,6 +11,10 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: RAK");
+    // conn.write("Move: up");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50)
   });
   return conn;
 };
